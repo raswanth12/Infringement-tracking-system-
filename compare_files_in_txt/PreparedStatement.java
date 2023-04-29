177be56d365b649e745895fcb1e2c44ce3cfeaa6
Raswanth CB <83648180+raswanth12@users.noreply.github.com>
2023-04-29 09:23:08 +0530

    have to check

---
+++ compare_files_in_txt/PreparedStatement.java
@@ -0,0 +1,12 @@
+package compare_files_in_txt;
+
+
+public class PreparedStatement {
+
+    public void setString(int i, String fileContent) {
+    }
+
+    public void executeUpdate() {
+    }
+
+}
